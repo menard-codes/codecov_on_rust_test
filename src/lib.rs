5ba3eba992484ec50506474a9f9c08cf8f1e9ea9
Menard Maranan <menardmaranan.codes@gmail.com>
2022-07-21 16:48:54 +0800

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+pub fn calc(x: i32, y: i32, op: &str) -> i32 {
+    match op {
+        "+" => x + y,
+        "-" => x - y,
+        "*" => x * y,
+        "/" => x / y,
+        _ => panic!("Unknown operator")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::calc;
+
+    #[test]
+    fn it_can_add() {
+        assert_eq!(calc(5, 6, "+"), 11);
+    }
+
+    #[test]
+    fn it_can_subtract() {
+        assert_eq!(calc(10, 6, "-"), 4);
+    }
+
+    #[test]
+    fn it_can_multiply() {
+        assert_eq!(calc(3, 4, "*"), 12);
+    }
+
+    #[test]
+    fn it_can_divide() {
+        assert_eq!(calc(20, 4, "/"), 5);
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_panics_with_unknown_operator() {
+        calc(4, 5, "%");
+    }
+}
